144c95bdd07f94216905ef7ab21b152a9904ab06
Alessandro Resta <alessandro.resta@gmail.com>
2017-04-22 10:15:08 +0100

    Implement Insert() to store data in DB

---
+++ storage/storage.go
@@ -0,0 +1,26 @@
+package storage
+
+import (
+	"log"
+
+	"github.com/fatih/structs"
+	bolt "github.com/johnnadratowski/golang-neo4j-bolt-driver"
+)
+
+// Insert data in Neo4j
+func Insert(stmt bolt.Stmt, data interface{}) error {
+
+	// Parses data struct to a map before sending it to ExeNeo()
+	result, err := stmt.ExecNeo(structs.Map(data))
+	if err != nil {
+		return err
+	}
+
+	numResult, err := result.RowsAffected()
+	if err != nil {
+		return err
+	}
+
+	log.Printf("CREATED ROWS: %d\n", numResult)
+	return nil
+}
