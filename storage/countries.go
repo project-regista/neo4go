d461531f1f25e1a704c70636ffe5651e59b039ac
Alessandro Resta <alessandro.resta@gmail.com>
2017-04-28 10:35:32 +0100

    CountriesStmt rewrite
    Remove CountriesStmt. Caller should iterate over the CountryStmt instead.

--- storage/countries.go
+++ storage/countries.go
@@ -6,39 +6,15 @@ import (
 	bolt "github.com/johnnadratowski/golang-neo4j-bolt-driver"
 )
 
-// TODO: Create queries
-const (
-	countryQuery              = "MERGE (n:NODE {foo: {foo}, bar: {bar}})"
-	countriesQuery            = "MERGE (n:NODE {foo: {foo}, bar: {bar}})"
-	countriesCompetitionQuery = "MERGE (n:NODE {foo: {foo}, bar: {bar}})"
-)
+const countryQuery = `MERGE (c:Country{id: {ID} })
+											ON CREATE SET c.name = {Name}, c.isoCode = {IsoCode}, c.flag = {Flag}
+											ON MATCH SET c.name = {Name}, c.isoCode = {IsoCode}, c.flag = {Flag}`
 
 // CountryStmt prepare a neo4j statement for single contry data
 func CountryStmt(conn bolt.Conn) (bolt.Stmt, error) {
-
 	stmt, err := conn.PrepareNeo(countryQuery)
 	if err != nil {
 		return nil, fmt.Errorf("Failed to create country statement: %s", err)
-	}
-	return stmt, nil
-}
-
-// CountriesStmt prepare a neo4j statement for a list countries
-func CountriesStmt(conn bolt.Conn) (bolt.Stmt, error) {
-
-	stmt, err := conn.PrepareNeo(countriesQuery)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to create countries statement: %s", err)
-	}
-	return stmt, nil
-}
-
-// CountriesCompetitionsStmt prepare a neo4j statement for a list of countries w/ competition data
-func CountriesCompetitionsStmt(conn bolt.Conn) (bolt.Stmt, error) {
-
-	stmt, err := conn.PrepareNeo(countriesCompetitionQuery)
-	if err != nil {
-		return nil, fmt.Errorf("Failed to create countriesCompetitions statement: %s", err)
 	}
 	return stmt, nil
 }
